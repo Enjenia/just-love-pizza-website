08e7f906cdde6963ec6808a5fe1383e99ac8c1c7
Jeffrew123 <jenniferdon97@gmail.com>
2023-02-24 22:44:33 +0000

    Updated Landing.css
    Updated landing-body font colour, size and weight
    Updated nav font colour

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -46,16 +46,16 @@ const Landing = () => {
 				</section>
 				<nav className="landing-nav">
 					<span id="landing-nav-item-home" className="landing-nav-item">
-						Home
+						HOME
 					</span>
 					<span id="landing-nav-item-about" className="landing-nav-item" onClick={onABOUTTextClick}>
-						About
+						ABOUT
 					</span>
 					<span id="landing-nav-item-services" className="landing-nav-item" onClick={onSERVICESTextClick}>
-						Services
+						SERVICES
 					</span>
 					<span id="landing-nav-item-gallery" className="landing-nav-item" onClick={onGALLERYTextClick}>
-						Gallery
+						GALLERY
 					</span>
 				</nav>
 				<section className="landing-contact">
@@ -71,48 +71,6 @@ const Landing = () => {
 			</section>
 		</div>
 
-    // <div className="landing">
-    //   <div className="bg">
-    //     <img
-    //       className="andreasronningens2ysslw97l4uns-icon"
-    //       alt=""
-    //       src="../andreasronningens2ysslw97l4unsplash-1@2x.png"
-    //     />
-    //     <div className="landingrectangle1" data-scroll-to="landingRectangle" />
-    //   </div>
-    //   <div className="text">
-    //     <div className="we-are-a">
-    //       We are a pizza trailer service that caters Weddings, Birthday Parties,
-    //       Family Events, Corporate Events, Artisan Markets and more!
-    //     </div>
-    //     <b className="welcome-to-just">Welcome to Just Love Pizza</b>
-    //   </div>
-    //   <div className="logo">
-    //     <img
-    //       className="jlp-final-horiz-1"
-    //       alt=""
-    //       src="../jlp-final-horiz-1@2x.png"
-    //     />
-    //   </div>
-    //   <div className="nav">
-    //     <b className="gallery3" onClick={onGALLERYTextClick}>
-    //       GALLERY
-    //     </b>
-    //     <b className="services3" onClick={onSERVICESTextClick}>
-    //       <span className="services-txt1">
-    //         <p className="services4">SERVICES</p>
-    //       </span>
-    //     </b>
-    //     <b className="about2" onClick={onABOUTTextClick}>
-    //       ABOUT
-    //     </b>
-    //     <b className="home1">HOME</b>
-    //     <button className="contact1" onClick={onContactClick}>
-    //       <div className="contact-child" />
-    //       <b className="contact-us">CONTACT US</b>
-    //     </button>
-    //   </div>
-    // </div>
   );
 };
 
