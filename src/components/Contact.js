054e52d79919c087257cb437d593006b6b67d4b8
Jeffrew123 <jenniferdon97@gmail.com>
2023-02-27 12:44:49 +0000

    Updated Contact.scss
    Refactoring code for Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,23 +4,31 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="gallery1" data-scroll-to="gallery" />
-      <div className="contactmain">
-        <b className="justlovepizzacouk1">
-          <span className="justlovepizzacouk-txt">
-            <p className="justlovepizzacouk2">justlovepizza.co.uk</p>
-            <p className="blank-line">&nbsp;</p>
-          </span>
-        </b>
-        <b className="b">07868017133</b>
-        <b className="justlovepizzainfogmailcom1">
-          justlovepizza.info@gmail.com
-        </b>
-        <b className="get-in-touch">Get in touch</b>
-        <a href="https://www.instagram.com/just.love.pizza/?hl=en"><img className="instagram-icon" alt="" src="../instagram.svg" /></a>
-        <a href="https://twitter.com/Just_Love_Pizza"><img className="twitter-icon" alt="" src="../twitter.svg" /></a>
-        <a href="https://www.facebook.com/profile.php?id=100086715121290"><img className="facebook-icon" alt="" src="../facebook.svg" /></a>
+
+		<div className="contact-main">
+			<div className="contact-body">
+
+				<div className="contact-info">
+					<p className="get-in-touch">Get in touch</p>
+					<p className="website">justlovepizza.co.uk</p>
+					<p className="mobile">07868017133</p>
+					<p className="email">justlovepizza.info@gmail.com</p>
+				</div>
+
+				<div className="social-media-icons">
+					<a href="https://www.facebook.com/profile.php?id=100086715121290"><img className="facebook-icon" alt="" src="../facebook.svg" /></a>
+					<a href="https://twitter.com/Just_Love_Pizza"><img className="twitter-icon" alt="" src="../twitter.svg" /></a>
+					<a href="https://www.instagram.com/just.love.pizza/?hl=en"><img className="instagram-icon" alt="" src="../instagram.svg" /></a>
+				</div>
+
       </div>
-      <img className="jlp-oven-1" alt="" src="../jlp-oven-1@2x.png" />
+
+			<div className="contact-image">
+      	<img className="jlp-oven-1" alt="" src="../jlp-oven-1@2x.png" />
+			</div>
+
+		</div>
+
     </div>
   );
 };
