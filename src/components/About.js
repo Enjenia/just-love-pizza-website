bf699bd6b034565d7dc1173629f50491eb7be786
Jeffrew123 <jenniferdon97@gmail.com>
2023-05-10 12:34:12 +0100

    Updating elements with new content
    Client supplied photos and info to implement

--- src/components/About.js
+++ src/components/About.js
@@ -2,43 +2,85 @@ import { useCallback } from "react";
 import "./About.scss";
 
 const About = () => {
-  const onContactClick = useCallback(() => {
-    const anchor = document.querySelector("[data-scroll-to='contact-us']");
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+	const onContactClick = useCallback(() => {
+		const anchor = document.querySelector("[data-scroll-to='contact-us']");
+		if (anchor) {
+			anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+		}
+	}, []);
 
-  return (
-    <div className="about-main" data-scroll-to="about-main">
-      <section className="about-text">
-				<p className="about-title">A little bit about the business</p>
-        <p className="about-content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Sed ut
-          perspiciatis unde omnis iste natus error sit voluptatem accusantium
-          doloremque laud
-        </p>
-				<button className="about-contact-button button" onClick={onContactClick}>
-						GET IN TOUCH
-				</button>
-      </section>
-			<section className="placeholder">
-				<img
-        className="placeholder-image"
-        alt=""
-        src="../placeholderimage@2x.png"
-      	/>
+	return (
+		<div className="about-main" data-scroll-to="about-main">
+			<section className="about-text">
+				<div className="julie-paragraphs">
+					<div className="julie">
+						<img className="julie" alt="" src="../julie.png" />
+					</div>
+					<div className="about-text-top">
+						<p className="about-title">A little bit more about the business</p>
+						<p className="p1">
+							Welcome to our wood-fired mobile events pizza business, run by a
+							passionate husband and wife team!
+						</p>
+						<p className="p2">
+							We specialise in bringing the authentic taste of wood-fired pizza
+							to events of all sizes, whether it's a wedding, corporate event,
+							or private party. Our mobile setup allows us to cater to your
+							needs wherever you are, be it at a scenic outdoor location or in
+							the comfort of your own back garden. We can also be found in the
+							bustling atmosphere of artisan markets. Our pizzas are made using
+							only the freshest ingredients and cooked to perfection in minutes
+							in our custom-made wood-fired oven. The result is a crispy,
+							flavourful crust and toppings that burst with deliciousness.
+						</p>
+						<p className="p3">
+							We offer two types of service for our wood-fired pizzas. The first
+							is pizzas cooked to order, where guests can choose from our menu
+							and watch as their pizza is cooked to perfection in our wood-fired
+							oven. The second option is a buffet-style service, where we serve
+							a variety of pizzas and guests can help themselves to their
+							favourite slices.
+						</p>
+					</div>
+				</div>
+
+				<div className="middle">
+					<p className="p4">
+						In addition to our delicious wood-fired pizzas, we also offer a
+						small breakfast service, perfect for morning events or markets, all
+						made using high-quality ingredients and cooked in our wood-fired
+						oven.
+					</p>
+				</div>
+
+				<div className="lewis-paragraphs">
+					<div className="lewis-text">
+						<p className="p5">
+							As a husband and wife team, we take pride in delivering a
+							high-quality service that exceeds your expectations. We are
+							committed to ensuring your event is stress-free and enjoyable,
+							leaving you with unforgettable memories.
+						</p>
+						<p className="p6">
+							If you're looking for a unique and memorable way to cater your
+							event, look no further than our wood-fired mobile events pizza
+							business. Contact us today to discuss how we can make your event
+							one to remember!
+						</p>
+						<button
+							className="about-contact-button button"
+							onClick={onContactClick}
+						>
+							GET IN TOUCH
+						</button>
+					</div>
+					<div className="lewis">
+						<img className="lewis" alt="" src="../lewis.png" />
+					</div>
+				</div>
 			</section>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default About;
-
-
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,39 +2,35 @@ import { useCallback } from "react";
 import "./Landing.scss";
 
 const Landing = () => {
-  const onGALLERYTextClick = useCallback(() => {
-    const anchor = document.querySelector("[data-scroll-to='gallery']");
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+	const onGALLERYTextClick = useCallback(() => {
+		const anchor = document.querySelector("[data-scroll-to='gallery']");
+		if (anchor) {
+			anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+		}
+	}, []);
 
-  const onSERVICESTextClick = useCallback(() => {
-    const anchor = document.querySelector(
-      "[data-scroll-to='services-main']"
-    );
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+	const onSERVICESTextClick = useCallback(() => {
+		const anchor = document.querySelector("[data-scroll-to='services-main']");
+		if (anchor) {
+			anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+		}
+	}, []);
 
-  const onABOUTTextClick = useCallback(() => {
-    const anchor = document.querySelector(
-      "[data-scroll-to='about-main']"
-    );
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+	const onABOUTTextClick = useCallback(() => {
+		const anchor = document.querySelector("[data-scroll-to='about-main']");
+		if (anchor) {
+			anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+		}
+	}, []);
 
-  const onContactClick = useCallback(() => {
-    const anchor = document.querySelector("[data-scroll-to='contact-us']");
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+	const onContactClick = useCallback(() => {
+		const anchor = document.querySelector("[data-scroll-to='contact-us']");
+		if (anchor) {
+			anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+		}
+	}, []);
 
-  return (
+	return (
 		<div className="landing" data-scroll-to="landing">
 			<header className="landing-header">
 				<section className="landing-logo">
@@ -45,21 +41,40 @@ const Landing = () => {
 					/>
 				</section>
 				<nav className="landing-nav">
-					<span id="landing-nav-item-home" className="landing-nav-item" data-scroll-to="landing">
+					<span
+						id="landing-nav-item-home"
+						className="landing-nav-item"
+						data-scroll-to="landing"
+					>
 						HOME
 					</span>
-					<span id="landing-nav-item-about" className="landing-nav-item" onClick={onABOUTTextClick}>
+					<span
+						id="landing-nav-item-about"
+						className="landing-nav-item"
+						onClick={onABOUTTextClick}
+					>
 						ABOUT
 					</span>
-					<span id="landing-nav-item-services" className="landing-nav-item" onClick={onSERVICESTextClick}>
+					<span
+						id="landing-nav-item-services"
+						className="landing-nav-item"
+						onClick={onSERVICESTextClick}
+					>
 						SERVICES
 					</span>
-					<span id="landing-nav-item-gallery" className="landing-nav-item" onClick={onGALLERYTextClick}>
+					<span
+						id="landing-nav-item-gallery"
+						className="landing-nav-item"
+						onClick={onGALLERYTextClick}
+					>
 						GALLERY
 					</span>
 				</nav>
 				<section className="landing-contact">
-					<button className="landing-contact-button button" onClick={onContactClick}>
+					<button
+						className="landing-contact-button button"
+						onClick={onContactClick}
+					>
 						CONTACT US
 					</button>
 				</section>
@@ -67,11 +82,15 @@ const Landing = () => {
 
 			<section className="landing-body">
 				<h1 className="landing-body-title">Welcome to Just Love Pizza</h1>
-				<p className="landing-body-content">We are a pizza trailer service that caters Weddings, Birthday Parties, Family Events, Corporate Events, Artisan Markets and more!</p>
+				<p className="landing-body-content">
+					Craving wood-fired pizza for your event? Let us bring our wood-fired
+					pizza oven to you! Our passionate team will serve up a variety of
+					tasty pizzas buffet-style for your guests to enjoy. Contact us now to
+					elevate your event with our unforgettable wood-fired pizza.
+				</p>
 			</section>
 		</div>
-
-  );
+	);
 };
 
 export default Landing;
