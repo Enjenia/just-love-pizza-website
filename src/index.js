86024f1ce868374dd9379a6c89fd6c9f49e58582
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2023-05-11 14:06:19 +0100

    Updated the project to have our linting
    Updated the index to use strict mode.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import "./global.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import './global.scss';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
