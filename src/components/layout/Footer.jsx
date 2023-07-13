49e06d67cdc55c6345c61e34e7aeb3f4715f3bbd
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2023-07-13 17:46:38 +0100

    Updated the food hygiene rating image

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,7 +7,7 @@ import Truck from '../../images/truck.svg';
 import Question from '../../images/question.svg';
 import Logo from '../../images/jlp-final-horiz-2@2x.webp';
 import enjenia from '../../images/enjenia.webp';
-import FoodRating from '../../images/food-rating.svg';
+import FoodRating from '../../images/food-rating.jpg';
 
 const Footer = () => {
 	const handleClick = useCallback((location) => {
@@ -88,7 +88,12 @@ const Footer = () => {
 					</button>
 					<br />
 					<br />
-					<img className='icon' alt='Food hygiene rating 5 out of 5 Very good' src={FoodRating} loading='lazy' />
+					<img
+						className='footer-contact-food-rating'
+						alt='Food hygiene rating 5 out of 5 Very good'
+						src={FoodRating}
+						loading='lazy'
+					/>
 				</section>
 			</header>
 
